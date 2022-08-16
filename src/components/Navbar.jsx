b088ab92d9426ccbdec9671f2a8f25abcd694240
Sebastian Mosquera <sebimosquera@gmail.com>
2022-08-16 05:41:27 -0300

    nada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,8 @@ export const Navbar = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
+                    <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+                        <ul className="navbar-nav ">
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
